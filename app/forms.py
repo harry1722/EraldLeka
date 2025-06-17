29cca03045fed8d075bc596739b2e978e8c0b122
harry1722 <raldiraldi42@gmail.com>
2025-06-17 06:58:32 +0200

    Updated

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
